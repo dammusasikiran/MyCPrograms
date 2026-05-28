8edca35fdcdb452735a9a1ce63061a492e05869b
dammusasikiran <dammusasikiran@gmail.com>
2026-05-28 15:11:07 +0530

    Create tocalculatethetriangle.c

---
+++ tocalculatethetriangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the value\t");
+    scanf("%d %d %d",&a,&b,&c);
+     if(a+b>c&&b+c>a&&c+a>b)
+    {
+        printf("the given value is triangle");
+    }
+    else
+    {
+        printf("the values is invalid");
+    }
+    return 0;
+}
